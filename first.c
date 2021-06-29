c8ae6c70be6d4f5fa1a7e30e82c2a2d9bdae1a96
iqbalhossain <iqbalhossainsha2303@gmail.com>
2021-06-30 02:13:09 +0600

    first commit

---
+++ first.c
@@ -0,0 +1,16 @@
+#include<math.h>
+int main()
+{
+    double a,b,c,x1,x2,d;
+
+    printf("Enter 3 value of a,b & c\n");
+    scanf("%lf %lf %lf",&a,&b,&c);
+
+    d = sqrt(b*b - 4*a*c);
+    x1 = (-b + d)/(2*a);
+    x2 = (-b -d)/(2*a);
+
+    printf("X1 = %.2lf, X2 = %.2lf",x1, x2);
+    printf("Iqbal");
+
+}
